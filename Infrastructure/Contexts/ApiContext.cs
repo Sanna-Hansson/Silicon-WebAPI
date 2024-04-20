3b0df14c1f41c9bb1b67a1e83018c70641a30472
sanna-hansson <sanna-hansson@live.com>
2024-04-20 21:37:34 +0200

    Add project files.

---
+++ Infrastructure/Contexts/ApiContext.cs
@@ -0,0 +1,12 @@
+﻿using Infrastructure.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+
+namespace Infrastructure.Contexts;
+
+ public class ApiContext(DbContextOptions<ApiContext>options) : DbContext(options)
+    {
+        public DbSet<SubscriberEntity> Subscribers { get; set; }
+        public DbSet<CourseEntity> Courses { get; set; }
+    }
+
---
+++ Infrastructure/Entities/SubscriberEntity.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Infrastructure.Entities;
+
+public class SubscriberEntity
+{
+    [Required]
+    [Key]
+    public string Email { get; set; } = null!;
+
+
+}
